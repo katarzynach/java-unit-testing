5c2359cde222b61c2191de6dce55e10f3335a6c0
katarzynach <kasia97h@o2.pl>
2023-05-18 10:10:06 +0200

    Junit test challenge solutions

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -15,4 +15,27 @@ public class CalculatorTest {
         assertEquals(expected, actual);
     }
 
+    @Test
+    public void testSubstract(){
+        int actual = calculator.subtract(5,2);
+        assertEquals(3,actual);
+    }
+
+    @Test
+    public void testMultiply(){
+        int actual = calculator.multiply(2,2);
+        assertEquals(4, actual);
+    }
+
+    @Test
+    public void testDivide(){
+        int actual = calculator.divide(10,2);
+        assertEquals(5, actual);
+    }
+
+    @Test
+    public void testDivide_returnsInt(){
+        int actual = calculator.divide(3,2);
+        assertEquals(1, actual);
+    }
 }
